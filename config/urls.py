6ca661e9a28a1a33ddf36e00a1cdc33eaae7f9ef
Jacob Dick <jdick@solutionreach.com>
2019-09-11 07:32:36 -0600

    change name

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title='Document Service')
+schema_view = get_swagger_view(title='Reviews Service')
 
 urlpatterns = [
     path('', schema_view),
