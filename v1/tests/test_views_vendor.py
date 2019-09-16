0979dc9d0cc7ba05d3d23855673b418f0632fe8e
Jacob Dick <jdick@solutionreach.com>
2019-09-16 17:13:54 -0600

    add test for POST /v1/vendors

--- v1/tests/test_views_vendor.py
+++ v1/tests/test_views_vendor.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from django.test import TestCase
 from rest_framework.test import APIClient
 
@@ -18,3 +20,20 @@ class VendorViewTests(TestCase):
             'results': [],
         }
         self.assertDictEqual(request.data, expected_result)
+
+    def test_post_vendor(self):
+        """Should add a vendor and return vendor data"""
+        test_vendor_name                = 'Test Vendor 1'
+        test_vendor_integrations_type   = 'test_type'
+        test_vendor_integrations_id     = str(uuid4())
+        request = self.client.post(BASE_URL, {
+            'name': test_vendor_name,
+            'integrations_type': test_vendor_integrations_type,
+            'integrations_id': test_vendor_integrations_id,
+        })
+        self.assertTrue('id' in request.data)
+        self.assertEqual(test_vendor_name, request.data['name'])
+        self.assertEqual(test_vendor_integrations_type, request.data['integrations_type'])
+        self.assertEqual(test_vendor_integrations_id, request.data['integrations_id'])
+        self.assertTrue('created_at' in request.data)
+        self.assertTrue('updated_at' in request.data)
