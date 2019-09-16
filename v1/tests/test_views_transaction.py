b68f61a3cab1c814b1df85c5590ac554f47703bb
Jacob Dick <jdick@solutionreach.com>
2019-09-16 08:08:45 -0600

    add test for no transactions

---
+++ v1/tests/test_views_transaction.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+
+BASE_URL = '/v1/transactions'
+
+class TransactionViewTests(TestCase):
+    def __init__(self, *args, **kwargs):
+        self.client = APIClient()
+        super().__init__(*args, **kwargs)
+
+    def test_no_transactions(self):
+        """Ensures that an empty response (no data existing) looks as expected"""
+        request         = self.client.get(BASE_URL)
+        expected_result = {
+            'count': 0,
+            'next': None,
+            'previous': None,
+            'results': [],
+        }
+        self.assertDictEqual(request.data, expected_result)
