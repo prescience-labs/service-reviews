d626bc0732478bdd60e3b0502141aa87e79ae9b7
Jacob Dick <jdick@solutionreach.com>
2019-09-17 16:04:20 -0600

    added doctring to get_or_none

--- common/models/managers.py
+++ common/models/managers.py
@@ -3,6 +3,7 @@ from django.db.models import ObjectDoesNotExist
 
 class GetOrNoneManager(models.Manager):
     def get_or_none(self, **kwargs):
+        """Returns the object if it exists or None if it doesn't"""
         try:
             return self.get(**kwargs)
         except ObjectDoesNotExist:
