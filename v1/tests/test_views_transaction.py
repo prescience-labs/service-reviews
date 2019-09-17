afc4b3efd058916cad1994fc619f3a541d9edcde
Jacob Dick <jdick@solutionreach.com>
2019-09-17 16:08:55 -0600

    add test for POST /v1/transactions/comprehensive with multiple products

--- v1/tests/test_views_transaction.py
+++ v1/tests/test_views_transaction.py
@@ -30,8 +30,10 @@ class CreateTransactionComprehensiveViewTests(TestCase):
         self.vendor_integrations_id     = '12345'
         self.vendor_integrations_type   = 'amazon'
         self.vendor_product_id          = uuid4()
+        self.vendor_product_id_2        = uuid4()
         self.vendor_name                = 'Test Vendor'
         self.product_name               = 'Test Product'
+        self.product_name_2             = 'Test Product 2'
         self.vendor                     = Vendor.objects.create(name=self.vendor_name,
                                                                 integrations_id=self.vendor_integrations_id,
                                                                 integrations_type=self.vendor_integrations_type,
@@ -40,6 +42,11 @@ class CreateTransactionComprehensiveViewTests(TestCase):
         self.inventory                  = Inventory.objects.create(vendor=self.vendor,
                                                                    product=self.product,
                                                                    vendor_product_id=self.vendor_product_id,
+                                                                   )
+        self.product_2                  = Product.objects.create(name=self.product_name_2)
+        self.inventory_2                = Inventory.objects.create(vendor=self.vendor,
+                                                                   product=self.product_2,
+                                                                   vendor_product_id=self.vendor_product_id_2,
                                                                    )
 
     def test_post_transaction_comprehensive_with_all_data(self):
@@ -51,6 +58,20 @@ class CreateTransactionComprehensiveViewTests(TestCase):
             'vendor_integrations_id': self.vendor_integrations_id,
             'vendor_product_ids': [
                 self.vendor_product_id,
+            ],
+        })
+        self.assertEqual(request.status_code, 201)
+
+    def test_post_transaction_comprehensive_with_multiple_products(self):
+        """Should return a 201 Created"""
+        request = self.client.post(BASE_URL + '/comprehensive', {
+            'customer_email': self.customer_email,
+            'customer_phone': self.customer_phone,
+            'vendor_integrations_type': self.vendor_integrations_type,
+            'vendor_integrations_id': self.vendor_integrations_id,
+            'vendor_product_ids': [
+                self.vendor_product_id,
+                self.vendor_product_id_2,
             ],
         })
         self.assertEqual(request.status_code, 201)
@@ -159,3 +180,30 @@ class CreateTransactionComprehensiveViewTests(TestCase):
             ],
         })
         self.assertContains(request, 'integrations_type', status_code=400)
+
+    def test_post_transaction_comprehensive_with_invalid_vendor_integrations_id(self):
+        """Should return a 400 and contain relevant and helpful text"""
+        request = self.client.post(BASE_URL + '/comprehensive', {
+            'customer_email': self.customer_email,
+            'customer_phone': self.customer_phone,
+            'vendor_integrations_type': self.vendor_integrations_type,
+            'vendor_integrations_id': 'invalid_id',
+            'vendor_product_ids': [
+                self.vendor_product_id,
+            ],
+        })
+        self.assertContains(request, 'integrations_id', status_code=400)
+
+    def test_post_transaction_comprehensive_with_invalid_vendor_product_ids(self):
+        """Should return a 400 and contain relevant and helpful text"""
+        request = self.client.post(BASE_URL + '/comprehensive', {
+            'customer_email': self.customer_email,
+            'customer_phone': self.customer_phone,
+            'vendor_integrations_type': self.vendor_integrations_type,
+            'vendor_integrations_id': self.vendor_integrations_id,
+            'vendor_product_ids': [
+                'invalid_product_id_1',
+                'invalid_product_id_2',
+            ],
+        })
+        self.assertContains(request, 'vendor_product_id', status_code=400)
