8dfa6e30f8a537d442f9ba4832e02be9576a2d3b
Jacob Dick <jdick@solutionreach.com>
2019-09-17 23:51:00 -0600

    add ENVIRONMENT env var

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ import dj_database_url
 from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
-
+ENVIRONMENT                 = os.getenv('ENVIRONMENT', 'development')
 BASE_DIR                    = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY                  = os.getenv('SECRET_KEY')
 DEBUG                       = os.getenv('DEBUG', 'false').lower() == 'true'
