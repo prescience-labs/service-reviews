68db608d083e3c715275155c9b9868caa946d1d1
Jacob Dick <jdick@solutionreach.com>
2019-09-18 16:01:44 -0600

    change admin site title

--- common/admin.py
+++ common/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 
 from common.models import Product, Review, Transaction, Vendor
 
--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,11 @@ from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_swagger_view(title='Reviews Service')
 
+# Admin site settings
+admin.site.site_title   = 'Reviews admin'
+admin.site.site_header  = 'Reviews administration'
+admin.site.index_title  = 'Reviews administration'
+
 urlpatterns = [
     path('', schema_view),
     path('v1', include('v1.urls')),
