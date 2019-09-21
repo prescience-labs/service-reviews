4198037124b99f6ef4681c6b47f4536727383322
Jacob Dick <jdick@solutionreach.com>
2019-09-21 13:54:42 -0600

    make product and transaction fields not required

--- v1/serializers/review.py
+++ v1/serializers/review.py
@@ -3,21 +3,11 @@ from rest_framework import serializers
 from common.models import Review
 
 class ReviewSerializer(serializers.ModelSerializer):
+    product     = serializers.UUIDField(required=False, allow_null=True, allow_blank=True)
+    transaction = serializers.UUIDField(required=False, allow_null=True, allow_blank=True)
     class Meta:
         model               = Review
-        fields              = [
-            'id',
-            'vendor',
-            'text',
-            'transaction',
-            'product',
-            'rating',
-            'rating_max',
-            'analytics_id',
-            'sentiment_analysis',
-            'created_at',
-            'updated_at',
-        ]
+        fields              = '__all__'
         read_only_fields    = [
             'sentiment_analysis',
             'created_at',
