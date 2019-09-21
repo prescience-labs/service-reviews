395b0828605c88ceae42345195e36ce004cfb561
Jacob Dick <jdick@solutionreach.com>
2019-09-21 16:41:00 -0600

    fix reviews POST to allow foregoing the product and transaction fields

--- v1/serializers/review.py
+++ v1/serializers/review.py
@@ -3,8 +3,11 @@ from rest_framework import serializers
 from common.models import Review
 
 class ReviewSerializer(serializers.ModelSerializer):
-    # product     = serializers.CharField(required=False, allow_null=True, allow_blank=True)
-    # transaction = serializers.CharField(required=False, allow_null=True, allow_blank=True)
+    # Must set default=None on these because they are part of a unique_together constraint
+    # https://github.com/encode/django-rest-framework/issues/4456#issuecomment-244017057
+    product     = serializers.CharField(required=False, allow_null=True, default=None)
+    transaction = serializers.CharField(required=False, allow_null=True, default=None)
+
     class Meta:
         model               = Review
         fields              = '__all__'
