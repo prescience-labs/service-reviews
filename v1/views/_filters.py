c740a563f8f2c2ba3f8537624bf0ba3c872818b0
Jacob Dick <jdick@solutionreach.com>
2019-09-21 18:34:28 -0600

    add help text to filters

--- v1/views/_filters.py
+++ v1/views/_filters.py
@@ -1,12 +1,13 @@
+from django.utils.translation import gettext_lazy as _
 from django_filters import rest_framework as filters
 
 from common.models import Product, Review, Vendor
 
 class ProductFilter(filters.FilterSet):
-    vendor_product_id       = filters.CharFilter(field_name='inventory__vendor_product_id', lookup_expr='icontains')
-    vendor_product_id_exact = filters.CharFilter(field_name='inventory__vendor_product_id', lookup_expr='exact')
-    name                    = filters.CharFilter(field_name='name', lookup_expr='icontains')
-    name_exact              = filters.CharFilter(field_name='name', lookup_expr='exact')
+    vendor_product_id       = filters.CharFilter(field_name='inventory__vendor_product_id', lookup_expr='icontains', help_text=_('Case-insensitive fuzzy `vendor_product_id` search'))
+    vendor_product_id_exact = filters.CharFilter(field_name='inventory__vendor_product_id', lookup_expr='exact', help_text=_('Exact `vendor_product_id` search'))
+    name                    = filters.CharFilter(field_name='name', lookup_expr='icontains', help_text=_('Case-insensitive fuzzy `name` search'))
+    name_exact              = filters.CharFilter(field_name='name', lookup_expr='exact', help_text=_('Exact `name` search'))
 
     class Meta:
         model   = Product
@@ -18,7 +19,9 @@ class ProductFilter(filters.FilterSet):
         ]
 
 class ReviewFilter(filters.FilterSet):
-    text = filters.CharFilter(field_name='text', lookup_expr='icontains')
+    text    = filters.CharFilter(field_name='text', lookup_expr='icontains', help_text=_('Case-insensitive fuzzy text search'))
+    before  = filters.DateFilter(field_name='created_at', lookup_expr='lte', help_text=_('**Exclusive** before date (YYYY-MM-DD)'))
+    after   = filters.DateFilter(field_name='created_at', lookup_expr='gte', help_text=_('**Inclusive** after date (YYYY-MM-DD)'))
 
     class Meta:
         model   = Review
@@ -27,12 +30,12 @@ class ReviewFilter(filters.FilterSet):
         ]
 
 class VendorFilter(filters.FilterSet):
-    integrations_type       = filters.CharFilter(field_name='integrations_type', lookup_expr='icontains')
-    integrations_type_exact = filters.CharFilter(field_name='integrations_type', lookup_expr='exact')
-    integrations_id         = filters.CharFilter(field_name='integrations_id', lookup_expr='icontains')
-    integrations_id_exact   = filters.CharFilter(field_name='integrations_id', lookup_expr='exact')
-    name                    = filters.CharFilter(field_name='name', lookup_expr='icontains')
-    name_exact              = filters.CharFilter(field_name='name', lookup_expr='exact')
+    integrations_type       = filters.CharFilter(field_name='integrations_type', lookup_expr='icontains', help_text=_('Case-insensitive fuzzy `integrations_type` search'))
+    integrations_type_exact = filters.CharFilter(field_name='integrations_type', lookup_expr='exact', help_text=_('Exact `integrations_type` search'))
+    integrations_id         = filters.CharFilter(field_name='integrations_id', lookup_expr='icontains', help_text=_('Case-insensitive fuzzy `integrations_id` search'))
+    integrations_id_exact   = filters.CharFilter(field_name='integrations_id', lookup_expr='exact', help_text=_('Exact `integrations_id` search'))
+    name                    = filters.CharFilter(field_name='name', lookup_expr='icontains', help_text=_('Case-insensitive fuzzy `name` search'))
+    name_exact              = filters.CharFilter(field_name='name', lookup_expr='exact', help_text=_('Exact `name` search'))
 
     class Meta:
         model   = Vendor
