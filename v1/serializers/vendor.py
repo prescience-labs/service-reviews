c69dbc70a7476bf0ec8c1cdef846b4c9c64f361b
Jacob Dick <jdick@solutionreach.com>
2019-10-15 08:23:08 -0600

    add automatic team creation on vendor creation

--- v1/serializers/vendor.py
+++ v1/serializers/vendor.py
@@ -1,15 +1,22 @@
+import logging
+
+from django.conf import settings
 from django.db import IntegrityError
 from django.db.models import ObjectDoesNotExist
 from django.utils.translation import gettext_lazy as _
+import requests
 from rest_framework import serializers
 
 from common.models import Inventory, Product, Vendor
+
+logger = logging.getLogger(__name__)
 
 class VendorSerializer(serializers.ModelSerializer):
     # Must set default=None on these because they are part of a unique_together constraint
     # https://github.com/encode/django-rest-framework/issues/4456#issuecomment-244017057
     integrations_type   = serializers.CharField(required=False, allow_null=True, default=None)
     integrations_id     = serializers.CharField(required=False, allow_null=True, default=None)
+    team_id             = serializers.CharField(required=False, default=None)
 
     class Meta:
         model               = Vendor
@@ -19,6 +26,26 @@ class VendorSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
         ]
+
+    def create(self, validated_data):
+        team_id = validated_data.get('team_id', None)
+        team    = None
+        if team_id:
+            logger.debug(f"team_id {team_id} included in the request; checking if it is a valid team")
+            team = requests.get(f"{settings.AUTH_SERVICE['BASE_URL']}/teams/{team_id}")
+        if not team_id or not team:
+            logger.debug(f"team_id: {team_id}; team: {team}")
+            logger.debug('Team not found. Creating a new team for this vendor.')
+            response = requests.post(f"{settings.AUTH_SERVICE['BASE_URL']}/teams",
+                json={
+                    'name': f"{validated_data['name']}'s Team",
+                },
+            )
+            team_id = response.json()['id']
+        validated_data['team_id'] = team_id
+        logger.debug(f'Creating a vendor: {validated_data}')
+        vendor = super().create(validated_data)
+        return vendor
 
 class VendorProductSerializer(serializers.ModelSerializer):
     vendor_product_id = serializers.SerializerMethodField(help_text=_("A value representing the vendor's ID number for the product."))
@@ -52,3 +79,6 @@ class VendorProductSerializer(serializers.ModelSerializer):
         vendor_id   = self.context['vendor_id'] if self.context['vendor_id'] else None
         item        = Inventory.objects.get(vendor__id=vendor_id, product=obj)
         return item.vendor_product_id
+
+    def create(self, validated_data):
+        return super().create(validated_data)
