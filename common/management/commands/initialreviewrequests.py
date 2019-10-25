f2c34cce658c82b6eb2612089e9e3aaebc1875e9
Jacob Dick <jdick@solutionreach.com>
2019-10-25 11:17:46 -0600

    add try/except to initialreviewrequests management command

--- common/management/commands/initialreviewrequests.py
+++ common/management/commands/initialreviewrequests.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+import logging
 
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
@@ -6,6 +7,8 @@ import requests
 
 from common.models import Transaction
 from common.services.email import Email
+
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = "Sends the first review request for all transactions that haven't sent a review request"
@@ -15,9 +18,13 @@ class Command(BaseCommand):
         if len(transactions) is 0:
             self.stdout.write(self.style.NOTICE('No transactions were found with the given criteria'))
         for t in transactions:
-            # send email
-            email = Email(t.customer_email)
-            email.send_review_request(t.vendor.name, transaction_id=t.id)
+            try:
+                # send email
+                email = Email(t.customer_email)
+                email.send_review_request(t.vendor.name, transaction_id=t.id)
+            except:
+                self.stdout.write(self.style.ERROR(f"Error on sending an email to {t.customer_email} for transaction {t.id}"))
+                continue
 
             # increment review_requests_sent
             t.review_requests_sent += 1
