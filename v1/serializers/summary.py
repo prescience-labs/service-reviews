2f63079ce2c00ee06aa0d039be76e29b1cc56eda
Jacob Dick <jdick@solutionreach.com>
2019-11-03 07:40:53 -0500

    add file for summary serializers

---
+++ v1/serializers/summary.py
@@ -0,0 +1,9 @@
+"""Summary serializers
+
+These serializers are custom-built to provide concise summaries of large
+datasets including all of queried resources.
+
+e.g.
+- all transactions for a date range
+- review request stages
+"""
